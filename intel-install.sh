7ff198c8d119d9a30fbac1a785f6825e6f041768
Ali Ahmed <54253787+Breakpower@users.noreply.github.com>
2023-05-15 23:11:10 +0300

    Create intel-install.sh

---
+++ intel-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}[*] install casaos toolkit intel${NC}"
+echo -e "${GREEN}[*] Setting Up everything${NC}"
+echo -e "${GREEN}[*] Installing Essentials${NC}"
+sleep 5
+cat /etc/apt/sources.list | grep "bullseye main contrib non-free" || sudo sed -i "s/bullseye main/bullseye main contrib non-free/g" /etc/apt/sources.list
+apt-get update
+apt-get install -y build-essential
+apt-get install -y gcc
+apt-get install -y git
+apt-get install -y vim 
+apt-get install -y wget 
+apt-get install -y curl
+apt-get install -y python3
+apt-get install -y python-pip 
+apt-get install -y dnsutils
+apt-get install -y nano
+apt-get install -y htop
+apt-get install -y lshw
+apt-get install -y neofetch
+apt-get install -y sudo
+apt update
+echo " ${GREEN}[*] Setting up Casaos${NC} "
+sleep 5 
+curl -fsSL https://get.casaos.io | sudo bash
